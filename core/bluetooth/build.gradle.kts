4146e8985aa0737c538870c29a5ac44d40669199
Dawid Seroka <dawid.seroka@untitledkingdom.com>
2023-04-06 11:35:39 +0200

    init :core:bluetooth

---
+++ core/bluetooth/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("android.library")
+    id("android.hilt")
+}
+
+android {
+    namespace = "com.myproject.gymphysique.core.bluetooth"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kable.android.ble)
+    implementation(libs.timber)
+}
